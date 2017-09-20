fc4bbfe3443f526cd0cad219b6ad12a3b506d948
David de Lusenet <lusenet@momkai.com>
2017-09-20 12:51:50 +0200

    Small update to webpack setup

--- config/base.js
+++ config/base.js
@@ -6,21 +6,12 @@ module.exports = function() {
   return {
     context,
     output: {
-      path: resolve(__dirname, './dist/js'),
+      path: resolve(__dirname, './../dist/js'),
       filename: '[name].min.js',
       publicPath: '/js/'
     },
     resolve: {
-      modules: ['node_modules', 'src/app/components'],
-      alias: {
-        'TweenLite': resolve('node_modules', 'gsap/src/uncompressed/TweenLite.js'),
-        'TweenMax': resolve('node_modules', 'gsap/src/uncompressed/TweenMax.js'),
-        'TimelineLite': resolve('node_modules', 'gsap/src/uncompressed/TimelineLite.js'),
-        'TimelineMax': resolve('node_modules', 'gsap/src/uncompressed/TimelineMax.js'),
-        'ScrollMagic': resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/ScrollMagic.js'),
-        'animation.gsap': resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js'),
-        'debug.addIndicators': resolve('node_modules', 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js')
-      }
+      modules: ['node_modules', 'src/app/components']
     },
     module: {
       rules: [
